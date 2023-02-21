c7d1a9841bf6f3a074d7d7deb7022c974d2ca9a7
liang-bruce <liangjitian777@gmail.com>
2023-02-22 09:33:36 +1100

    add README

--- MoodDiary/Mooddiary.swift
+++ MoodDiary/Mooddiary.swift
@@ -7,16 +7,16 @@
 
 import Foundation
 
-struct MoodDiary : Identifiable, Codable, Hashable {
-    var id: UUID = UUID()
+struct MoodDiary : Codable, Hashable {
+//    var id: UUID = UUID() // needed if struct is Identifiable
     var date: String
     var value: Int
 }
 
-extension MoodDiary {
-    static let list : [MoodDiary] = [
-        MoodDiary(date: "2023-02-01", value: 1),
-        MoodDiary(date: "2023-02-02", value: 2),
-        MoodDiary(date: "2023-02-03", value: 3),
-    ]
-}
+//extension MoodDiary {
+//    static let list : [MoodDiary] = [
+//        MoodDiary(date: "2023-02-01", value: 1),
+//        MoodDiary(date: "2023-02-02", value: 2),
+//        MoodDiary(date: "2023-02-03", value: 3),
+//    ]
+//}
---
+++ README.md
@@ -0,0 +1,16 @@
+# ContentView:
+The "test screen"
+
+
+# Storage:
+The underlying functions, don't worry about it
+
+
+# MoodDiaryStorage:
+The observable object to be used in ContentView
+
+Have some wrapper function for easier use, detailed comment in file
+
+
+# Mooddiary:
+The MoodDiary struct (Model)
